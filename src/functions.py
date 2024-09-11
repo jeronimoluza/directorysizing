369263da20a98eb75e53a5b4c9427e2032183ffa
jeronimol <jeronimol@iadb.org>
2024-09-11 13:11:18 -0400

    added file_id and dir_id to summaries

--- src/functions.py
+++ src/functions.py
@@ -62,6 +62,9 @@ def analyze_directories(directories, size_threshold, units):
     # Convert size_threshold to bytes using the unit_multiplier
     size_threshold_in_bytes = size_threshold * unit_multiplier[units]
 
+    dir_id = 1  # Unique ID counter for directory summary
+    file_id = 1  # Unique ID counter for file summary
+
     for directory in directories:
         assert os.path.isdir(directory), "Directory %s does not exist" % directory
         total_size_bytes = 0
@@ -86,6 +89,7 @@ def analyze_directories(directories, size_threshold, units):
                 # Collect file-level data
                 file_summary.append(
                     {
+                        "id": file_id,
                         "file_name": file,
                         "file_size_bytes": file_size_bytes,
                         "file_size_h": utils.human_readable_size(file_size_bytes),
@@ -94,10 +98,12 @@ def analyze_directories(directories, size_threshold, units):
                         "source_folder": directory,
                     }
                 )
+                file_id += 1
 
         # Append directory-level summary
         dir_summary.append(
             {
+                "id": dir_id,
                 "directory": directory,
                 "file_count": file_count,
                 "total_size_bytes": total_size_bytes,
@@ -105,5 +111,6 @@ def analyze_directories(directories, size_threshold, units):
                 "large_file_count": large_file_count,
             }
         )
+        dir_id += 1
 
     return dir_summary, file_summary
